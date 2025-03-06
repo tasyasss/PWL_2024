a1bd4022909c6b1bd00e790bc7e7ba6224252d4b
tasyasss <tasyasakina0703@gmail.com>
2025-03-06 08:53:08 +0700

    js 2 - prak 1 - awal

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'selamat datang';
+});
+
+Route::get('/about', function () {
+    return 'SIB 2C - 2341760131 - An Naastasya S.';
 });
