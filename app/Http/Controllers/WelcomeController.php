89059ec2c7ea4c61f82cd9a61b5d4565b5de67b5
tasyasss <tasyasakina0703@gmail.com>
2025-03-08 00:17:05 +0700

    js 2 - prak 3 - meneruskan data ke view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,9 +10,16 @@ class WelcomeController extends Controller
         return 'Hello World';
     }
 
-    // Menampilkan View dari Controller
+    // // Menampilkan View dari Controller
+    // public function greeting(){
+    //     return view('blog.hello', ['name' => 'Natha']) 
+    //         . '<br>(menampilkan view dari controller)';
+    // }
+
+    // Meneruskan data ke view
     public function greeting(){
-        return view('blog.hello', ['name' => 'Natha']) 
-            . '<br>(menampilkan view dari controller)';
+        return view('blog.hello')
+            ->with('name','Natha')
+            ->with('occupation','Astronaut');
     }
 }
--- resources/views/blog/hello.blade.php
+++ resources/views/blog/hello.blade.php
@@ -3,7 +3,10 @@
 
 <body>
     <h1>Hello, {{ $name }}</h1>
-    <p>(dalam direktori views/blog)</p>
+    {{-- <p>(dalam direktori views/blog)</p> --}}
+
+    {{-- Meneruskan data ke view --}}
+    <h1>You are {{ $occupation }}</h1>
 </body>
 
 </html>
